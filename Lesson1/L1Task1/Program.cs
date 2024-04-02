2c09d6e19ba7b4d0f6cc01aa13c735af33cd6fd3
[mzhelskikh.y] <yam@pfsz.ru>
2024-04-02 17:04:08 +0700

    Добавлено решение задачи 1 к Уроку 1. Введение в ООП. Классы и объекты

--- Lesson1/L1Task1/Program.cs
+++ Lesson1/L1Task1/Program.cs
@@ -1,9 +1,84 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
+    
+    /*
+    Cоздайте проект по шаблону Console Application.
+    Требуется: 
+    - Создать класс с именем Rectangle.
+    В теле класса создать два поля, описывающие длины сторон double side1, side2.
+    Создать пользовательский конструктор Rectangle(double side1, double side2), в теле которого
+    поля side1 и side2 инициализируются значениями аргументов.
+    Создать два метода, вычисляющие площадь прямоугольника - double AreaCalculator() и периметр
+    прямоугольника - double PerimeterCalculator().
+    Создать два свойства double Area и double Perimeter с одним методом доступа get.
+    Написать программу, которая принимает от пользователя длины двух сторон прямоугольника и выводит
+    на экран периметр и площадь.
+     */
+    
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
+            Console.WriteLine("Введите длину стороны 1");
+            var side1AsString = Console.ReadLine();
+            var side1 = Convert.ToDouble(side1AsString);
+            
+            Console.WriteLine("Введите длину стороны 2");
+            var side2AsString = Console.ReadLine();
+            var side2 = Convert.ToDouble(side2AsString);
+            
+            var rect = new Rectangle(side1: side1, side2: side2);
+
+            var rectArea = rect.AreaCalculator();
+            var rectPerimeter = rect.PerimeterCalculator();
+            
+            Console.WriteLine($"Площадь прямоугольника: {rectArea} ед.кв., периметр прямоугольника: {rectPerimeter} ед.");
         }
+    }
+
+    internal class Rectangle
+    {
+        private double _side1, _side2;
+
+        public double Side1
+        {
+            get => _side1;
+            set {
+                if (value > 0)
+                {
+                    _side1 = value;
+                }
+            }
+        }
+        
+        public double Side2  {
+            get => _side2;
+            set {
+                if (value > 0)
+                {
+                    _side2 = value;
+                }
+            }
+        }
+
+        public Rectangle(double side1, double side2)
+        {
+            Side1 = side1;
+            Side2 = side2;
+        }
+
+        public double AreaCalculator()
+        {
+            return Side1 * Side2;
+        }
+        
+        public double PerimeterCalculator()
+        {
+           return Side1 * 2 + Side2 * 2;
+        }
+
     }
 }
