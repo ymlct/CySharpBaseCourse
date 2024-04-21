df2f43e346535c75f85efd3c6075447b6bea86ec
[mzhelskikh.y] <yam@pfsz.ru>
2024-04-22 06:36:44 +0700

    Добавлены задание 2 к уроку 7, задание 1 к уроку 8

--- Lesson7/L7Task2/Program.cs
+++ Lesson7/L7Task2/Program.cs
@@ -1,12 +1,14 @@
-﻿namespace L7Task2
+﻿using System;
+
+namespace L7Task2
 {
     /*
     Создайте класс MyClass и структуру MyStruct, которые содержат в себе поля public string change.
     В классе Program создайте два метода:
     - static void ClassTaker(MyClass myClass), который присваивает полю change экземпляра
-    myClass значение «изменено».
+      myClass значение «изменено».
     - static void StruktTaker(MyStruct myStruct), который присваивает полю change экземпляра
-    myStruct значение «изменено».
+      myStruct значение «изменено».
     Продемонстрируйте разницу в использовании классов и структур, создав в методе Main() экземпляры
     структуры и класса. Измените, значения полей экземпляров на «не изменено», а затем вызовите методы
     ClassTaker и StruktTaker. Выведите на экран значения полей экземпляров. Проанализируйте
@@ -16,6 +18,43 @@
     {
         public static void Main(string[] args)
         {
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+            
+            myClass.Change = "не изменено";
+            myStruct.Change = "не изменено";
+
+            Console.WriteLine($"До передачи в метод: MyClass: hashCode={myClass.GetHashCode()}, {myClass.Change}");
+            Console.WriteLine($"До передачи в метод: MyStruct: hashCode={myStruct.GetHashCode()}, {myStruct.Change}");
+            
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+            
+            Console.WriteLine($"После передачи в метод: MyClass: hashCode={myClass.GetHashCode()}, {myClass.Change}");
+            Console.WriteLine($"После передачи в метод: MyStruct: hashCode={myStruct.GetHashCode()}, {myStruct.Change}");
         }
+
+
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.Change = "изменено";
+            Console.WriteLine($"В момент исполнения метода: MyClass: hashCode={myClass.GetHashCode()}, {myClass.Change}");
+        }
+
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.Change = "изменено";
+            Console.WriteLine($"В момент исполнения метода: MyStruct: hashCode={myStruct.GetHashCode()}, {myStruct.Change}");
+        }
+    }
+
+    internal class MyClass
+    {
+        public string Change;
+    }
+    
+    internal struct MyStruct
+    {
+        public string Change;
     }
 }
--- Lesson8/L8Task1/Program.cs
+++ Lesson8/L8Task1/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace L8Task1
+﻿using System;
+
+namespace L8Task1
 {
     /*
     Задание 2
-    Используя Visual Studio, создайте проект по шаблону Console Application.
+    Создайте проект по шаблону Console Application.
     Создайте статический класс с методом void Print (string stroka, int color), который выводит на
     экран строку заданным цветом. Используя перечисление, создайте набор цветов, доступных
     пользователю. Ввод строки и выбор цвета предоставьте пользователю.
@@ -11,6 +13,57 @@
     {
         public static void Main(string[] args)
         {
+
+            Console.WriteLine("Цвета, доступные для выбора:");
+            var colors = Enum.GetValues(typeof(Colors));
+            foreach (var color in colors)
+            {
+                Console.WriteLine($"{color} ({(int) color})");
+            }
+           
+            Console.WriteLine("Введите строку");
+            var inputString = Console.ReadLine();
+            
+            Console.WriteLine("Введите код цвета");
+            var colorCode = Console.ReadLine();
+
+            Int32.TryParse(colorCode, out int colorIntCode);
+
+            if (colorIntCode == 0)
+            {
+                Console.WriteLine($"Введены неверные данные.");
+                return;
+            }
+
+            StringPrinter.Print(inputString, colorIntCode);
         }
+    }
+
+    internal static class StringPrinter
+    {
+        internal static void Print(string stroka, int color)
+        {
+            var name = Enum.GetName(typeof(Colors), color);
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine($"Цвет с кодом {color} не найден.");
+                return;
+            }
+            
+            var consoleColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), name);
+            
+            Console.BackgroundColor = consoleColor;
+            Console.WriteLine(stroka);
+        }
+    }
+
+    internal enum Colors
+    {
+        Blue = 9,
+        Green = 10,
+        Red = 12,
+        Yellow = 13,
+        White = 14
     }
 }
