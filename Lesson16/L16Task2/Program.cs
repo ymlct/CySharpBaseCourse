0cc7696c5c5f1a2726a2323685122b72bd6f7a64
[mzhelskikh.y] <yam@pfsz.ru>
2024-05-15 06:33:10 +0700

    Добавлены решения задач для урока 16

--- Lesson16/L16Task2/Program.cs
+++ Lesson16/L16Task2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace L16Task2
+﻿using System;
+
+namespace L16Task2
 {
     /*
     Задание 3
@@ -10,6 +12,71 @@
     {
         public static void Main(string[] args)
         {
+
+            House house1 = new House(
+                new Address("Россия", "Кемерово", "Притомский проспект", "11"),
+                15
+                );
+
+            House house2 = house1.Clone();
+            
+            House house3 = house1.DeepClone();
+
+            Console.WriteLine("house1: houseHash={0}, addressHash={1}", house1.GetHashCode(), house1.Address.GetHashCode());
+            Console.WriteLine("house2: houseHash={0}, addressHash={1}", house2.GetHashCode(), house2.Address.GetHashCode());
+            Console.WriteLine("house3: houseHash={0}, addressHash={1}", house3.GetHashCode(), house3.Address.GetHashCode());
+        }
+    }
+
+    internal class House
+    {
+        public Address Address { get; set; }
+        
+        public int AmountOfFloors { get; set; }
+        
+
+        public House(Address address, int amountOfFloors)
+        {
+            Address = address;
+            AmountOfFloors = amountOfFloors;
+        }
+
+        // Поверхностное копирование - создание новой ссылки на имеющийся экземпляр.
+        public House Clone()
+        {
+            return this;
+        }
+        
+        // Глубокое копирование - создание нового экземпляра данного класса и новых экземпляров для всех его полей,
+        // представленных ссылочным типом.
+        public House DeepClone()
+        {
+            return new House(Address.Clone() as Address, AmountOfFloors);
+        }
+
+    }
+    
+    internal class Address : ICloneable
+    {
+        public string Country;
+        
+        public string City;
+        
+        public string Street;
+        
+        public string Number;
+
+        public Address(string country, string city, string street, string number)
+        {
+            Country = country;
+            City = city;
+            Street = street;
+            Number = number;
+        }
+        
+        public object Clone()
+        {
+            return new Address(country: Country, city: City, street: Street, number: Number);
         }
     }
 }
--- Lesson16/L16Task3/Program.cs
+++ Lesson16/L16Task3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace L16Task3
+﻿using System;
+
+namespace L16Task3
 {
     /*
     Задание 4
@@ -10,6 +12,51 @@
     {
         public static void Main(string[] args)
         {
+
+            Date date1 = new Date(2024, 5, 14);
+            Date date2 = new Date(2024, 5, 12);
+            
+            var diff = date1 - date2;
+            Console.WriteLine(diff);
+            
+            var date3 = date1 + 2;
+            Console.WriteLine("{0}.{1}.{2}", date3.Year, date3.Month, date3.Day);
+        }
+    }
+
+    internal class Date
+    {
+        private readonly DateTime _date;
+
+        public int Year => _date.Year;
+        public int Month => _date.Month;
+        public int Day => _date.Day;
+        
+        public Date(int year, int month, int day)
+        {
+            _date = new DateTime(year, month, day);
+        }
+
+        public static int operator -(Date date1, Date date2)
+        {
+            if (date1._date > date2._date)
+            {
+                TimeSpan timeSpan = date1._date - date2._date;
+                return timeSpan.Days;
+            } 
+            if (date1._date < date2._date)
+            {
+                TimeSpan timeSpan = date2._date - date1._date;
+                return timeSpan.Days;
+            }
+            return 0;
+        }
+        
+        public static Date operator +(Date date, int days)
+        {
+            var newDate = date._date.AddDays(days);
+            
+            return new Date(newDate.Year, newDate.Month, newDate.Day);
         }
     }
 }
